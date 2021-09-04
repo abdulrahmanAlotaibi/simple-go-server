2a8c9c1231f4fd831212360360c4173b73f3ebb2
Abdulrahman Alotaibi <abdulrahman.dev.97@gmail.com>
2021-09-04 23:43:01 +0300

    add

---
+++ main.go
@@ -0,0 +1,19 @@
+package main 
+
+import "fmt"
+
+
+func main (){
+	fmt.Println("Hello World")
+	fmt.Println("Hello")
+
+	var x string = "Abdulrahman"
+	
+	
+	if(true){
+		const x = 2 
+		fmt.Print(x)
+	}
+	
+	fmt.Println(x)
+}
